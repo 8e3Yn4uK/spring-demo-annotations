56eba962deb29aa095bad2880f363c955cde9a9c
Oleksandr <sashko.kondratenko@gmail.com>
2019-02-05 18:38:53 +0100

    autowirring

--- src/com/springdemo/TennisCoach.java
+++ src/com/springdemo/TennisCoach.java
@@ -6,15 +6,27 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 
+    @Autowired
     private FortuneService fortuneService;
 
     public TennisCoach() {
+        System.out.println(">> TennisCoach: inside default constructor");
     }
 
+    /*
+    @Autowired
+    public void doSomeCrazyStuff(FortuneService fortuneService) {
+        System.out.println(">> TennisCoach: inside doSomeCrazyStuff() method");
+        this.fortuneService = fortuneService;
+    }
+    */
+
+    /*
     @Autowired
     public TennisCoach(FortuneService fortuneService) {
         this.fortuneService = fortuneService;
     }
+    */
 
     @Override
     public String getDailyWorkout() {
